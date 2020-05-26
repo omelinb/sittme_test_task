c6280537d824356b185be072bd3a738a4c05148c
omelinb <elros.ob@gmail.com>
2020-05-27 01:50:21 +0300

    Added pagination

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,8 +1,21 @@
+require 'pry'
+
 class Api::V1::PostsController < ApplicationController
+  before_action :page_params, only: [:index]
+
+  # GET /api/v1/posts
   def index
-    render json: Post.all
+    render json: Post.page(@limit, @cursor)
   end
 
   def create
   end
+
+  private
+
+  def page_params
+    @limit = (params[:limit] || 10).to_i
+    @cursor = (params[:cursor] || 0).to_i
+  end
 end
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  scope :page, -> (limit, cursor) {
+    order(:created_at).where('id > ?', cursor).limit(limit)
+  }
 end
