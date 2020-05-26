c8cf6d1f93248f1b90bc28c264af95d31c45f3ac
omelinb <elros.ob@gmail.com>
2020-05-26 04:26:55 +0300

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 Post.destroy_all
-Post.create(title: 'First post' ,text: 'This is my first post! Hahay!')
-Post.create(title: 'Second post' ,text: 'And this is second. Nothing interesting!')
-Post.create(title: 'Third post' ,text: 'The third one.')
+
+Post.create(title: 'First post' , text: 'This is my first post! Hahay!')
+Post.create(title: 'Second post' , text: 'And this is second. Nothing interesting!')
+Post.create(title: 'Third post' , text: 'The third one.')
+
+# And many other boring posts
+(4..50).each do |i|
+  Post.create(title: "Post number #{i}", text: "Text of post number #{i}.")
+end
