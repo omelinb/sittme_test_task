01e1a1ce2d89e0a287ccd14fe05a31f57a8de08d
omelinb <elros.ob@gmail.com>
2020-05-26 04:28:18 +0300

    Removed unneccessary line

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
       resources :posts, only: [:index, :create]
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
