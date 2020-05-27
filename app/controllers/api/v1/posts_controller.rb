65da5849b65add60be52d5dfe07d1a77c11538c9
omelinb <elros.ob@gmail.com>
2020-05-27 03:55:07 +0300

    Added create action

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,13 +2,21 @@ require 'pry'
 
 class Api::V1::PostsController < ApplicationController
   before_action :page_params, only: [:index]
+  before_action :post_params, only: [:create]
 
   # GET /api/v1/posts
   def index
     render json: Post.page(@limit, @cursor)
   end
 
+  # POST /api/v1/posts
   def create
+    post = Post.create(post_params)
+    if post.save
+      render json: post, status: :created
+    else
+      render json: { errors: post.errors }, status: :unprocessable_entity
+    end
   end
 
   private
@@ -16,6 +24,11 @@ class Api::V1::PostsController < ApplicationController
   def page_params
     @limit = (params[:limit] || 10).to_i
     @cursor = (params[:cursor] || 0).to_i
+  end
+
+  def post_params
+    binding.pry
+    params.require(:post).permit(:title, :text)
   end
 end
 
