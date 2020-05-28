8213af4d06cca5583a880df2ee915151d543d4dc
omelinb <elros.ob@gmail.com>
2020-05-28 06:38:07 +0300

    Added serialization and tests gems; refactored seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,8 @@
 Post.destroy_all
 
-Post.create(title: 'First post' , text: 'This is my first post! Hahay!')
-Post.create(title: 'Second post' , text: 'And this is second. Nothing interesting!')
-Post.create(title: 'Third post' , text: 'The third one.')
-
-# And many other boring posts
-(4..50).each do |i|
-  Post.create(title: "Post number #{i}", text: "Text of post number #{i}.")
+50.times do |i|
+  Post.create(
+    title: "Boring post number #{i}",
+    text: Faker::Lorem.paragraph
+  )
 end
