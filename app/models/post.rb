8f31ab30b265e9ecf04b85bcac2cfc2d64178699
omelinb <elros.ob@gmail.com>
2020-05-29 08:48:56 +0300

    Added order by

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-  scope :page, -> (page, per_page) { offset(page * per_page).limit(per_page) }
+  scope :page, -> (page, per_page) do
+    order(:created_at).offset(page * per_page).limit(per_page)
+  end
 end
