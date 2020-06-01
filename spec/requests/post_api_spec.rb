368a44a584f15e2b8267cd5ee731b24c1f16e4ca
omelinb <elros.ob@gmail.com>
2020-06-01 11:09:37 +0300

    Added tests

--- spec/requests/post_api_spec.rb
+++ spec/requests/post_api_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe 'Post API', type: :request do
   context 'post creation' do
@@ -11,19 +12,37 @@ RSpec.describe 'Post API', type: :request do
 
   context 'posts pagination' do
     let!(:posts) { create_list :post, 50 }
+    let(:first) { posts.first }
+    let(:last) { posts.last }
 
-    before { get '/api/v1/posts' }
+    it 'returns empty data' do
+      get api_v1_posts_path
 
-    it 'returns specific count of posts' do
-      expect(JSON.parse(response.body)['data'].size).to eq(10)
+      expect(JSON.parse(response.body)['data'].size).to eq(0)
     end
 
-    it "doesn't return link to previous page" do
-      expect(JSON.parse(response.body)['links'].key?('prev_page')).to eq(false)
+    it 'returns specific count of posts' do
+      get api_v1_posts_path('page[before]' => last.id, 'page[size]' => 5)
+
+      expect(JSON.parse(response.body)['data'].size).to eq(5)
+    end
+
+    it 'returns correct count of posts in range' do
+      get api_v1_posts_path('page[before]' => last.id, 'page[after]' => first.id)
+
+      expect(JSON.parse(response.body)['data'].size).to eq(posts.size - 2)
     end
 
     it 'returns link to next page' do
+      get api_v1_posts_path('page[after]' => first.id)      
+
       expect(JSON.parse(response.body)['links'].key?('next_page')).to eq(true)
+    end
+
+    it 'returns link to prev page' do
+      get api_v1_posts_path('page[before]' => last.id)      
+
+      expect(JSON.parse(response.body)['links'].key?('prev_page')).to eq(true)
     end
   end
 end
